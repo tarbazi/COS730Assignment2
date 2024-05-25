2d1ce02fd3066eeb6c441d7efc4be9acd1209c16
Bazie521 <MTHBAZ001@myuct.ac.za>
2024-05-25 16:36:33 +0200

    Added logic to the user class

--- NetscapeNavigatorBackend/src/main/java/com/example/netscapenavigatorbackend/API/Model/User.java
+++ NetscapeNavigatorBackend/src/main/java/com/example/netscapenavigatorbackend/API/Model/User.java
@@ -4,27 +4,45 @@ import java.util.ArrayList;
 public class User {
     String userName;
     String password;
+    String phoneNumber;
     ArrayList<String> visitedLinks;
 
-    public User(String userName, String password){
+    public User(String userName, String password, String phoneNumber){
         this.userName = userName;
         this.password = password;
+        this.phoneNumber = phoneNumber;
         visitedLinks = new ArrayList<>();
-    }
+    } //new user constructor
 
     public void setUserName(String userName) {
         this.userName = userName;
-    }
+    } //userName set method
 
-    public void setPassword(String password) {
+    private void setPassword(String password) {
         this.password = password;
-    }
+    } //password set method
+
+    public String changePassword(String oldPassword, String newPassword){
+        if (oldPassword.equals(password)){
+            setPassword(newPassword);
+            return "ACK";
+        }
+        else{
+            return "{ Response : NAK}";
+        }
+    } //set password upon user password change
+
+    public String passwordReset(){
+        String newPassword = Integer.toString((int)Math.random()*10000);
+        setPassword(newPassword);
+        return newPassword;
+    } //reset user password
 
     public void addLink(String visitedLink){
         visitedLinks.add(visitedLink);
-    }
+    } //add visited link to the users visited links list
 
     public void deleteLink(int pos){
-        visitedLinks.remove(pos);
-    }
+        visitedLinks.remove(pos); 
+    } //deletes history element in position pos
 }
